95205c0397d131dcd4a43e975f3caaf6ed7f0fbb
Metigator <metigator@gmail.com>
2021-07-12 11:06:06 -0400

    initial commit

---
+++ CAAssemblies/Program.cs
@@ -0,0 +1,27 @@
+﻿using DemoLib;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace CAAssemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = typeof(Program);
+            var assembly = type.Assembly;
+
+            //var stream = assembly.GetManifestResourceStream("CAAssemblies.data.countries.json");
+            var stream = assembly.GetManifestResourceStream(type, "data.countries.json");
+            var data = new BinaryReader(stream).ReadBytes((int) stream.Length);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write((char) data[i]);
+                System.Threading.Thread.Sleep(300);
+            }
+            stream.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DemoLib/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace DemoLib
+{
+    public class Demo
+    {
+        public static void Trace()
+        {
+            Console.WriteLine("Tracing...");
+            Console.WriteLine($"Executing Assembly: {Assembly.GetExecutingAssembly()}");
+            Console.WriteLine($"Entry Assembly: {Assembly.GetEntryAssembly()}");
+            Console.WriteLine($"Calling Assembly: {Assembly.GetCallingAssembly()}");
+        }
+    }
+}
